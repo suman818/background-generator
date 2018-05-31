4fb12710d2ca908791c0c8b041066d42d75260ad
suman818 <suman818@gmail.com>
2018-05-31 19:26:20 +0530

    package.json updated

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var _ = require('lodash');
+
+console.log(_);
+
+var array = [1,2,3,4,5,6,7];
+	  console.log('answer', _.without(array,5));
+
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById('color2');
 var body = document.querySelector('body');
